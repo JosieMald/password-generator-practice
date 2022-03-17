9f49e66c3571852eef3ac02a752ae4f381eb464b
Lucre <josielindamaldonado@gmail.com>
2022-03-17 17:05:46 -0400

    finished if statements for generating password.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var letters = ["abcdefghijklmnopqrstuvwxyz"];
-var specialCharacters = ["~!@#$%^&*()-"];
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var specialCharacters = "~!@#$%^&*()-";
 var count = 0;
+var randomPassword = [];
 
 (function passwordLength() {
   var pwLength = prompt(
@@ -27,29 +28,62 @@ function customization(pwLength) {
   if (upperCase == true) {
     count++;
   }
-  let numbers = confirm("Would you like to use numbers?");
-  if (numbers == true) {
+  let num = confirm("Would you like to use numbers?");
+  if (num == true) {
     count++;
   }
   let specialChar = confirm("Would you like to use special Characters?");
   if (specialChar == true) {
     count++;
   }
+  
   if (count == 0) {
     alert("Please choose character customization options.");
     customization();
   } else {
     console.log("This is the count: " + count);
-    generatePassword(pwLength);
+    generatePassword(pwLength, lowerCase, upperCase, num, specialChar);
   }
 }
 
-// var options = Math.floor(pwLength/count);
-// console.log(options);
-// var remainder = pwLength%count;
-// console.log(remainder);
-// var originalNum = (count * options) + remainder;
-// console.log(originalNum);
+function generatePassword(pwLength, lowerCase, upperCase, num, specialChar) {
+  var letter = letters.split("");
+  var specialCharacter = specialCharacters.split("");
+  var options = Math.floor(pwLength / count);
+  console.log("this is the pwlength: " + pwLength);
+  console.log("This is the options: " + options);
+  var remainder = pwLength % count;
+  console.log("This is the remainder: " + remainder);
+  var originalNum = count * options + remainder;
+  console.log("This is count * options + remainder: " + originalNum);
+  console.log("this is the length: " + letters.length);
+  if (lowerCase == true) {
+    for (var i = 0; i < options; i++) {
+      let x = Math.floor(Math.random() * letter.length);
+      randomPassword.push(letter[x]);
+    }
+  }
+  if (upperCase == true) {
+    for (var i = 0; i < options; i++) {
+      let x = Math.floor(Math.random() * letter.length);
+      randomPassword.push(letter[x].toUpperCase());
+    }
+  }
+  if (num == true) {
+    for (var i = 0; i < options; i++) {
+      let x = Math.floor(Math.random() * numbers.length);
+      randomPassword.push(numbers[x]);
+    }
+  }
+  if (specialChar == true) {
+    for (var i = 0; i < options; i++) {
+      let x = Math.floor(Math.random() * specialCharacter.length);
+      randomPassword.push(specialCharacter[x]);
+      console.log(specialCharacter[x]);
+    }
+    console.log(randomPassword);
+  }
+};
 
 // Write password to the #password input
 function writePassword() {
